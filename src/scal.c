961e0390ff7321939827099e0c405eaeda30ee1d
Han Chen <chen1324+git@gmail.com>
2018-06-12 13:49:51 +0800

    sample debug log.

--- src/scal.c
+++ src/scal.c
@@ -18,22 +18,38 @@
 
 
 #include <gtk/gtk.h>
+#include <gdk/gdk.h>
 #include <gio/gio.h>
 
+gboolean
+status_icon_button_release(GtkStatusIcon *status_icon,
+							GdkEvent *event,
+							gpointer user_data) {
+	g_debug("status_icon_button_release");
+
+
+}
 
 GNotification *
-init_notification(void) {
+init_notification(GtkWidget *window) {
 	GNotification *notifi;
+	GtkStatusIcon *status_icon;
 	GFile *icon_file;
+
+	g_debug("init_notification");
 
 	notifi = g_notification_new("Hi Notifi");
 
 	g_notification_set_title(notifi, "Title");
 	g_notification_set_body(notifi, "Hello world");
 
-	icon_file = g_file_new_for_path("./res/scal2.png");
+	icon_file = g_file_new_for_path("../res/scal2.png");
 	g_notification_set_icon(notifi, G_ICON(g_file_icon_new(icon_file)));
-	// FIXME: free icon/iconf_file
+	status_icon = gtk_status_icon_new_from_gicon(G_ICON(g_file_icon_new(icon_file)));
+	gtk_status_icon_set_screen(status_icon, gdk_screen_get_default());
+	g_signal_connect(status_icon, "button-release-event",
+		G_CALLBACK(status_icon_button_release), (gpointer)window);
+	// FIXME: free icon/icon_file
 	return notifi;
 }
 
@@ -45,12 +61,10 @@ activate(GtkApplication *app,
 	window = gtk_application_window_new(app);
 	gtk_window_set_title(GTK_WINDOW(window), "Hello GNOME");
 
-	GNotification * notifi = init_notification();
+	GNotification * notifi = init_notification(window);
 	g_application_send_notification(G_APPLICATION(app), "new-window", notifi);
 
 	gtk_widget_show_all(window);
-
-
 }
 
 
